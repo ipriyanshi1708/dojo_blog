60f02c913d28b74475dccef977badf52e4226090
Priyanshi Agarwal <priyanshiagarwal1708@gmail.com>
2023-04-19 14:12:34 +0530

    Completed the blog website

---
+++ src/BlogDetails.js
@@ -0,0 +1,32 @@
+import {useParams, useHistory} from "react-router-dom";
+import useFetch from "./useFetch";
+
+const BlogDetails = () => {
+    const {id} = useParams();
+    const {data: blog,isPending,error} = useFetch('http://localhost:8000/blogs/' + id);
+    const history=useHistory();
+
+    const handleDelete=()=>{
+        fetch('http://localhost:8000/blogs/'+blog.id,{
+            method: 'DELETE'
+        }).then(()=>{
+            history.push('/');
+        })
+    }
+    return ( 
+        <div className="blog-details">
+            {error && <div>{error}</div>}
+            {isPending && <div>Loading...</div> }
+            {blog && (
+                <article>
+                    <h2>{blog.title}</h2>
+                    <p>{blog.author}</p>
+                    <div>{blog.body}</div>
+                    <button onClick={handleDelete}>Delete</button>
+                </article>
+            )}
+        </div>
+     );
+}
+ 
+export default BlogDetails;
